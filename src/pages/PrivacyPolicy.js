954cc4fc43e8057d35b58649da43b11838ecd2db
Guruswamy <Guruswamy.Gowda@metavaratechnologies.com>
2026-02-11 17:09:28 +0530

    Created responsive gallery grid

---
+++ src/pages/PrivacyPolicy.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="policy-container">
+      <h1>Privacy Policy</h1>
+      <p>
+        
+      </p>
+
+      <h3>Information We Collect</h3>
+      <p>Coming Soon</p>
+
+      <h3>How We Use Information</h3>
+      <p>Coming Soon</p>
+
+      <h3>Contact Us</h3>
+      <p>Coming Soon</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
