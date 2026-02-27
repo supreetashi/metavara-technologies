ac3067374871804e87461e89911022aa3d917d47
krishna teja vemulapalli <129947036+KrishnaTeja0002@users.noreply.github.com>
2026-02-27 16:32:09 +0530

    Delete backend directory

--- backend/leads/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import LeadCreateView
-
-urlpatterns = [
-    path('contact/', LeadCreateView.as_view(), name='contact-lead-create'),
-]
--- backend/metavara/authentication.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.authentication import TokenAuthentication
-
-
-class BearerTokenAuthentication(TokenAuthentication):
-    keyword = "Bearer"
